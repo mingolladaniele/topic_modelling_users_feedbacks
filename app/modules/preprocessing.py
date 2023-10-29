7529339c4963e8eb2b44b829730b168f17507840
Daniele Mingolla <danielemingolla98ta@gmail.com>
2023-10-29 18:47:56 +0100

    add bert

--- app/modules/preprocessing.py
+++ app/modules/preprocessing.py
@@ -4,7 +4,17 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
+
 def preprocessing_row(text):
+    """
+    Preprocess a text by removing stopwords, punctuation, numbers, and certain custom words.
+
+    Args:
+        text (str): Input text to be preprocessed.
+
+    Returns:
+        str: Preprocessed text.
+    """
     custom_stopwords = [
         "user",
         "recommends",
@@ -28,16 +38,34 @@ def preprocessing_row(text):
 
     doc = nlp(text)
     cleaned_words = []
-    
+
     for token in doc:
         # Remove numbers and verbs
-        if not (token.is_stop or token.is_punct or token.like_num or token.pos_ == "VERB") and token.lemma_.lower() not in custom_stopwords:
+        if (
+            not (
+                token.is_stop
+                or token.is_punct
+                or token.like_num
+                or token.pos_ == "VERB"
+            )
+            and token.lemma_.lower() not in custom_stopwords
+        ):
             cleaned_words.append(token.lemma_)
-    
+
     cleaned_text = " ".join(cleaned_words)
     return cleaned_text
 
+
 def preprocess_text(input_file):
+    """
+    Preprocess text data from an input CSV file.
+
+    Args:
+        input_file (str): Path to the input CSV file.
+
+    Returns:
+        pd.DataFrame: Processed data as a DataFrame.
+    """
 
     # Read the input CSV
     data = pd.read_csv(input_file, delimiter="|")
@@ -47,5 +75,5 @@ def preprocess_text(input_file):
 
     # Apply custom preprocessing to the text data
     data["text"] = data["text"].apply(preprocessing_row)
-    
+
     return data
