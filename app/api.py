c3938f30f3128173ffa8643419bdfce47d21828d
Daniele Mingolla <danielemingolla98ta@gmail.com>
2023-10-29 18:47:56 +0100

    add bert

--- app/api.py
+++ app/api.py
@@ -1,16 +1,20 @@
-# app/api.py
 from flask import Flask, request, jsonify
 from modules.preprocessing import preprocess_text
-from modules.clustering import cluster_data_nmf
+from modules.clustering import cluster_data_nmf, cluster_data_bert
 import os
 from datetime import datetime
-
 
 app = Flask(__name__)
 
 
 @app.route("/topic_modeling", methods=["POST"])
 def topic_modeling():
+    """
+    Perform topic modeling on text data and save the results.
+
+    Returns:
+        JSON response with the path to the saved result file.
+    """
     if request.method == "POST":
         # Check if "input_df_path" and "model_name" parameters are in the request
         if "input_df_path" not in request.args:
@@ -20,10 +24,6 @@ def topic_modeling():
 
         input_df_path = request.args.get("input_df_path")
         model_name = request.args.get("model_name")
-        # Check if the model_name is valid (e.g. 'nmf')
-        if model_name not in ["nmf"]:
-            return jsonify({"error": "Invalid model_name"}), 400
-
         # Check if the input file name exists
         if not os.path.exists(input_df_path):
             return jsonify({"error": "Input file not found"}), 404
@@ -33,16 +33,21 @@ def topic_modeling():
         # Load the model based on model_name (implement model loading)
         if model_name == "nmf":
             clustered_data = cluster_data_nmf(data)
+        else:
+            model_name = "bert"
+            # BERT is used as the default model
+            clustered_data = cluster_data_bert(data)
 
         # Generate a timestamp
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
         # Modify the result filename to include the timestamp
-        result_file = os.path.join("./data/output/", f"output_{timestamp}.csv")
+        result_file = os.path.join(
+            "./data/output/", f"output__{model_name}_{timestamp}.csv"
+        )
         clustered_data.to_csv(result_file, index=False)
 
         return jsonify({"result_path": f"Results saved in: {result_file}"})
-
     else:
         return jsonify({"error": "Not a POST request!"}), 404
 
--- app/modules/clustering.py
+++ app/modules/clustering.py
@@ -5,8 +5,137 @@ from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import CountVectorizer
 from gensim.corpora import Dictionary
 import numpy as np
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import umap
+import hdbscan
+import os
+
+
+def c_tf_idf(documents, m, ngram_range=(1, 1)):
+    """
+    Compute the c-TF-IDF (normalized term frequency-inverse document frequency) matrix.
+
+    Args:
+        documents: List of text documents.
+        m: Total number of documents.
+        ngram_range: The n-gram range for the CountVectorizer.
+
+    Returns:
+        The computed c-TF-IDF matrix and the CountVectorizer object.
+    """
+    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(
+        documents
+    )
+    t = count.transform(documents).toarray()
+    w = t.sum(axis=1)
+    tf = np.divide(t.T, w)
+    sum_t = t.sum(axis=0)
+    idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+    tf_idf = np.multiply(tf, idf)
+    return tf_idf, count
+
+
+def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+    """
+    Extract the top n words per topic from the c-TF-IDF matrix.
+
+    Args:
+        tf_idf: The c-TF-IDF matrix.
+        count: The CountVectorizer object.
+        docs_per_topic: DataFrame with documents grouped by topics.
+        n: Number of top words to extract per topic.
+
+    Returns:
+        A dictionary containing the top n words for each topic.
+    """
+    words = count.get_feature_names_out()
+    labels = list(docs_per_topic.Topic)
+    tf_idf_transposed = tf_idf.T
+    indices = tf_idf_transposed.argsort()[:, -n:]
+    top_n_words = {
+        label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1]
+        for i, label in enumerate(labels)
+    }
+    return top_n_words
+
+
+def extract_topic_sizes(df):
+    """
+    Extract the sizes of each topic in the DataFrame.
+
+    Args:
+        df: DataFrame with clustered data.
+
+    Returns:
+        DataFrame with the size of each topic.
+    """
+    topic_sizes = (
+        df.groupby(["Topic"])
+        .text.count()
+        .reset_index()
+        .rename({"Topic": "Topic", "text": "Size"}, axis="columns")
+        .sort_values("Size", ascending=False)
+    )
+    return topic_sizes
+
+
+def cluster_data_bert(df):
+    """
+    Cluster data using the BERT sentence embeddings.
+
+    Args:
+        df: DataFrame containing text data.
+
+    Returns:
+        DataFrame with clustered data.
+    """
+    model_dir = "./sentence_transformer_model"
+    if not os.path.exists(model_dir):
+        # Download the model if it doesn't exist
+        model = SentenceTransformer("distilbert-base-nli-mean-tokens")
+        model.save(model_dir)
+    else:
+        # Load the model from the existing directory
+        model = SentenceTransformer(model_dir)
+
+    embeddings = model.encode(df["text"], show_progress_bar=False)
+
+    umap_embeddings = umap.UMAP(
+        n_neighbors=15, n_components=10, metric="cosine"
+    ).fit_transform(embeddings)
+
+    cluster = hdbscan.HDBSCAN(
+        min_cluster_size=4, metric="euclidean", cluster_selection_method="eom"
+    ).fit(umap_embeddings)
+    docs_df = pd.DataFrame(df, columns=["text"])
+
+    docs_df["Topic"] = cluster.labels_
+    docs_df["Doc_ID"] = range(len(docs_df))
+    docs_per_topic = docs_df.groupby(["Topic"], as_index=False).agg({"text": " ".join})
+    tf_idf, count = c_tf_idf(docs_per_topic.text.values, m=len(df))
+    top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=5)
+    # we can use topic_sizes to view how frequent certain topics are
+    # topic_sizes = extract_topic_sizes(docs_df)
+
+    df["group_name"] = ["cluster " + str(cluster_id) for cluster_id in cluster.labels_]
+    df["group_description"] = [
+        ",".join([word for word, _ in top_n_words[cluster_id]])[:20] for cluster_id in cluster.labels_
+    ]
+    df.drop(columns=["text"], inplace=True)
+    return df
+
 
 def cluster_data_nmf(df):
+    """
+    Cluster data using Non-Negative Matrix Factorization (NMF).
+
+    Args:
+        df: DataFrame containing text data.
+
+    Returns:
+        DataFrame with clustered data.
+    """
     # TF-IDF Vectorization with improved parameters
     tfidf_vectorizer = TfidfVectorizer(
         max_df=0.7, max_features=50, lowercase=True, stop_words="english"
@@ -21,7 +150,7 @@ def cluster_data_nmf(df):
     best_score = -1
     best_num_clusters = 0
     for n_clusters in range(2, max_clusters + 1):
-        kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init='auto')
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init="auto")
         cluster_labels = kmeans.fit_predict(tfidf_matrix)
         score = silhouette_score(tfidf_matrix, cluster_labels)
         silhouette_scores.append(score)
@@ -30,8 +159,8 @@ def cluster_data_nmf(df):
             best_num_clusters = n_clusters
 
     # Perform clustering with the best number of clusters
-    kmeans = KMeans(n_clusters=best_num_clusters, random_state=0, n_init='auto')
-    df['cluster_id'] = kmeans.fit_predict(tfidf_matrix)
+    kmeans = KMeans(n_clusters=best_num_clusters, random_state=0, n_init="auto")
+    df["cluster_id"] = kmeans.fit_predict(tfidf_matrix)
     # Extract group names and descriptions using NMF (Non-Negative Matrix Factorization)
     num_topics = best_num_clusters
 
@@ -53,20 +182,21 @@ def cluster_data_nmf(df):
     for i in range(num_topics):
         # Extract top keywords for the group description
         topic_keywords = " ".join(
-            keywords[idx] for idx in np.argsort(nmf.components_[i])[-10:][::-1]
+            keywords[idx] for idx in np.argsort(nmf_topic_matrix.components_[i])[-10:][::-1]
         )
-
-        group_name = "Cluster " + str(i)  # Use NMF topic index as group name
-        group_description = f"Cluster {i} - {topic_keywords}"
+        
+        # Use NMF topic index as group name
+        group_name = "cluster " + str(i)
+        group_description = f"cluster {i} - {topic_keywords}"
 
         group_names_dict[i] = group_name
         group_descriptions_dict[i] = group_description
-    
+
     group_descriptions = [group_descriptions_dict[label] for label in df["cluster_id"]]
     group_names = [group_names_dict[label] for label in df["cluster_id"]]
 
     # Assign cluster names and unique group descriptions to the data
     df["group_name"] = group_names
     df["group_description"] = group_descriptions
-    df.drop(columns = ['text', 'cluster_id'], inplace = True)
+    df.drop(columns=["text", "cluster_id"], inplace=True)
     return df
